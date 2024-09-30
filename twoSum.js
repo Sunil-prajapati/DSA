c199994ab14d23853703fda2ac879c830b35a9c9
Sunil Prajapati <sunil.prajapati@statrys.com>
2024-09-30 17:34:15 +0530

    twosum

---
+++ twoSum.js
@@ -0,0 +1,9 @@
+// question [2,7,11,15] output should be sum of two number is equal to 9
+function twoSum(arr, sum) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] + arr[i + 1] === sum) {
+      return [i, i + 1];
+    }
+  }
+}
+console.log(twoSum([2, 7, 11, 15, 4, 4], 8));
