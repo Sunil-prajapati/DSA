f51f95538297c1bd226d26557645ee293a3a0aa9
sunilSupertal <sunil@supertal.io>
2024-10-17 09:27:04 +0530

    checking

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,4 @@
-// question [2,7,11,15] output should be sum of two number is equal to 9
+// question [2,7,11,15] output should be sum of two number is equal to 9 
 function twoSum(arr, sum) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] + arr[i + 1] === sum) {
